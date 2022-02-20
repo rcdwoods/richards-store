5ba5651a3d3b2ba12c910652bac67cfed3f085d2
Richard Nascimento <rcdwoods@gmail.com>
2022-02-20 18:07:23 -0300

    :white_check_mark: Add tests to Order

--- src/test/java/com/richards/store/model/OrderTest.java
+++ src/test/java/com/richards/store/model/OrderTest.java
@@ -37,4 +37,37 @@ class OrderTest {
 
         Assertions.assertThat(order.getItems()).hasSize(3);
     }
+
+    @Test
+    void mustCalculateCorrectFinalPrice() throws InvalidCpfException {
+        Order order = new Order("123.456.789-09");
+        order.addItem(new Item(1L, "Música", "CD", new BigDecimal("30"), new BigDecimal("1"), defaultDimension), 3);
+        order.addItem(new Item(2L, "Vídeo", "DVD", new BigDecimal("50"), new BigDecimal("1"), defaultDimension), 1);
+        order.addItem(new Item(3L, "Video", "VHS", new BigDecimal("10"), new BigDecimal("1"), defaultDimension), 2);
+        BigDecimal finalPrice = order.getFinalPrice();
+        Assertions.assertThat(finalPrice).isEqualTo(new BigDecimal("160"));
+    }
+
+    @Test
+    void mustCalculateCorrectFinalConsideringPercentageDiscount() throws InvalidCpfException, ExpiredCouponException {
+        Order order = new Order("123.456.789-09");
+        Coupon coupon = new Coupon("TEST_COUPON", 20, LocalDate.now());
+        order.addItem(new Item(1L, "Música", "CD", new BigDecimal("30"), new BigDecimal("1"), defaultDimension), 3);
+        order.addItem(new Item(2L, "Vídeo", "DVD", new BigDecimal("50"), new BigDecimal("1"), defaultDimension), 1);
+        order.addItem(new Item(3L, "Video", "VHS", new BigDecimal("10"), new BigDecimal("1"), defaultDimension), 2);
+        order.addDiscountCoupon(coupon);
+        BigDecimal finalPrice = order.getFinalPrice();
+        Assertions.assertThat(finalPrice).isEqualTo(new BigDecimal("128.0"));
+    }
+
+    @Test
+    void mustNotAddCouponAndThrowExceptionWhenItIsExpired() throws InvalidCpfException, ExpiredCouponException {
+        Order order = new Order("123.456.789-09");
+        Coupon coupon = new Coupon("TEST_COUPON", 20, LocalDate.now().minusDays(1));
+        order.addItem(new Item(1L, "Música", "CD", new BigDecimal("30"), new BigDecimal("1"), defaultDimension), 3);
+        Exception exception = assertThrows(ExpiredCouponException.class, () -> {
+            order.addDiscountCoupon(coupon);
+        });
+        Assertions.assertThat(exception.getMessage()).isEqualTo("Expired coupon.");
+    }
 }
