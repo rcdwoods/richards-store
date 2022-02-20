29f15acb5fefc0459faefa54da271667b0c052a0
Richard Nascimento <rcdwoods@gmail.com>
2022-02-20 18:03:01 -0300

    :hammer: Add dimension and weight to Item

--- src/main/java/com/richards/store/model/Item.java
+++ src/main/java/com/richards/store/model/Item.java
@@ -8,12 +8,16 @@ public class Item {
     private String type;
     private String name;
     private BigDecimal price;
+    private BigDecimal weight;
+    private Dimension dimension;
 
     public Item(Long id, String type, String name, BigDecimal price, BigDecimal weight, Dimension dimension) {
         this.id = id;
         this.type = type;
         this.name = name;
         this.price = price;
+        this.weight = weight;
+        this.dimension = dimension;
     }
 
     public Long getId() {
