2fd4f3ed3c24fcb0ddf5eb62e725cfb24dbc4efd
Richard Nascimento <rcdwoods@gmail.com>
2022-02-20 17:57:53 -0300

    :hammer: Create Order Object

---
+++ src/main/java/com/richards/store/model/Order.java
@@ -0,0 +1,29 @@
+package com.richards.store.model;
+
+import com.richards.store.excepion.ExpiredCouponException;
+import com.richards.store.excepion.InvalidCpfException;
+
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Order {
+
+    private Long id;
+    private Cpf cpf;
+    private List<OrderItem> orderItems = new ArrayList<>();
+    private Coupon discountCoupon;
+
+    public Order(String cpf) throws InvalidCpfException {
+        this.cpf = new Cpf(cpf);
+    }
+
+    public List<OrderItem> getItems() {
+        return this.orderItems;
+    }
+
+    public void addItem(Item item, Integer amount) {
+        orderItems.add(new OrderItem(item.getId(), item.getPrice(), amount));
+    }
+}
